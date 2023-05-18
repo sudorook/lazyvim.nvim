d0552e01ba3e3f4fb41ec01a475f5e7faf8f0595
sudorook <daemon@nullcodon.com>
2023-05-18 13:07:36 -0500

    use <leader> shortcuts for comments
    matches nerd-commenter shortcuts more closely
    
    may disable this...

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,5 +1,20 @@
 return {
   {
     "numToStr/Comment.nvim",
+    opts = {
+      toggler = {
+        line = "<leader>cc",
+        block = "<leader>cb",
+      },
+      opleader = {
+        line = "<leader>cc",
+        block = "<leader>cb",
+      },
+      extra = {
+        above = "<leader>c0",
+        below = "<leader>co",
+        eol = "<leader>cA",
+      },
+    },
   },
 }
