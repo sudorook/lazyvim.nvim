2f376b8ca6180d80048984e567146bb66bdec84c
sudorook <daemon@nullcodon.com>
2023-05-25 00:19:34 -0500

    re-enable textwrap via formatexpr when null-ls doesn't cover it

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -4,6 +4,19 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     dependencies = { "mason.nvim" },
     opts = function(_, opts)
+      require("lazyvim.util").on_attach(function(client, bufnr)
+        if client.name == "null-ls" then
+          if
+            not require("null-ls.generators").can_run(
+              vim.bo[bufnr].filetype,
+              require("null-ls.methods").lsp.FORMATTING
+            )
+          then
+            vim.bo[bufnr].formatexpr = nil
+          end
+        end
+      end)
+
       local nls = require("null-ls")
       return {
         root_dir = require("null-ls.utils").root_pattern(
