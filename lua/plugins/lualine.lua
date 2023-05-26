6c26bc614acf9dd2ae26e9d75604ec7972148df7
sudorook <daemon@nullcodon.com>
2023-05-26 13:57:02 -0500

    rely on theme="auto" default for lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,8 +1,6 @@
--- the opts function can also be used to change the default opts:
 return {
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
-    opts = { options = { theme = "nightfox" } },
   },
 }
