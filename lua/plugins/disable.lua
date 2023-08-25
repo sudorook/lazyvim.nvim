44bbf66088d66b8458a24ec21034a39efbe1859a
sudorook <daemon@nullcodon.com>
2023-08-24 22:04:54 -0500

    use mini.indentscope but disable animation

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -5,7 +5,6 @@ return {
   { "folke/noice.nvim", enabled = false },
   { "echasnovski/mini.comment", enabled = false },
   { "goolord/alpha-nvim", enabled = false },
-  { "echasnovski/mini.indentscope", enabled = false },
   { "echasnovski/mini.surround", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
 }
