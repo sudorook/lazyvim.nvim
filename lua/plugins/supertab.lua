68e567a22ee17c2b6cd2cd7104028dbcccb89244
sudorook <daemon@nullcodon.com>
2023-08-24 22:04:07 -0500

    supertab: tweak comments

--- lua/plugins/supertab.lua
+++ lua/plugins/supertab.lua
@@ -1,10 +1,13 @@
 return {
+  -- Use <tab> for completion and snippets (supertab)
+  -- first: disable default <tab> and <s-tab> behavior in LuaSnip
   {
     "L3MON4D3/LuaSnip",
     keys = function()
       return {}
     end,
   },
+  -- then: setup supertab in cmp
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
@@ -31,7 +34,7 @@ return {
           if cmp.visible() then
             cmp.select_next_item()
             -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-            -- they way you will only jump inside the snippet region
+            -- this way you will only jump inside the snippet region
           elseif luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
           elseif has_words_before() then
