94fc83fe6ec5d4c75ff711484872140af6238c36
sudorook <daemon@nullcodon.com>
2024-01-21 08:44:54 -0600

    alphabetize disable list

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,10 +1,10 @@
 return {
-  { "rcarriga/nvim-notify", enabled = false },
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "catppuccin/nvim", enabled = false },
-  { "folke/noice.nvim", enabled = false },
   { "echasnovski/mini.comment", enabled = false },
-  { "goolord/alpha-nvim", enabled = false },
-  { "echasnovski/mini.surround", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
+  { "echasnovski/mini.surround", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "goolord/alpha-nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
 }
