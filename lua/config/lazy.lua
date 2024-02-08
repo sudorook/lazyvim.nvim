ef000262a630232d73b55ce1fe09a4fbb540c4aa
sudorook <daemon@nullcodon.com>
2024-02-08 15:17:52 -0600

    enable eslint extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -52,7 +52,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.tex" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
-    -- { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     -- { import = "lazyvim.plugins.extras.test.core" },
     -- { import = "lazyvim.plugins.extras.ui.alpha" },
