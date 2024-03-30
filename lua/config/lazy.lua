f57de6284df28c3505d615909ea262954e0c41f0
sudorook <daemon@nullcodon.com>
2024-03-29 22:36:28 -0500

    minor text alignment for consistency

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,10 +15,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    {
-      "LazyVim/LazyVim",
-      import = "lazyvim.plugins",
-    },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.vscode" },
     -- { import = "lazyvim.plugins.extras.coding.codeium" },
