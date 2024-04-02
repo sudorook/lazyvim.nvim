e81bc591d3f9353af026d17f26966c100977e7ae
sudorook <daemon@nullcodon.com>
2024-04-02 13:26:06 -0500

    custom clangd config to set 127.0.0.1 instead of localhost

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -34,7 +34,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.symbols-outline" },
     { import = "lazyvim.plugins.extras.formatting.black" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.lang.clangd" },
+    -- { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "plugins.extras.lang.clangd" }, -- localhost not resolving, so hard-codd 127.0.0.1
     -- { import = "lazyvim.plugins.extras.lang.cmake" },
     -- { import = "lazyvim.plugins.extras.lang.docker" },
     -- { import = "lazyvim.plugins.extras.lang.elixir" },
